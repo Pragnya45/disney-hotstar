c3b0cd4225cc7ac504cb51daab6a10ad921df480
Pragnya sahu <pragnya110@gmail.com>
2024-02-29 08:42:42 +0530

    Paywall animation added

--- src/app/views/Subscribe/index.js
+++ src/app/views/Subscribe/index.js
@@ -5,6 +5,7 @@ import ImageView from "@/app/Components/Image";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import DoneIcon from "@mui/icons-material/Done";
+import { Overlay } from "antd/es/popconfirm/PurePanel";
 
 const logo = "/assets/icons/logo-d-plus.svg";
 
@@ -27,6 +28,113 @@ function Subscribe() {
     },
     {
       language: "Tamil",
+    },
+  ];
+  const imageList = [
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
+    },
+    {
+      image: "/assets/images/subscribe-1.webp",
+    },
+    {
+      image: "/assets/images/subscribe-2.webp",
+    },
+    {
+      image: "/assets/images/subscribe-3.webp",
+    },
+    {
+      image: "/assets/images/subscribe-4.webp",
+    },
+    {
+      image: "/assets/images/subscribe-5.webp",
     },
   ];
   const handleMenu = (e) => {
@@ -78,8 +186,98 @@ function Subscribe() {
             </OptionMenu>
           )}
         </DropdownWrapper>
-        {/* <CreatableSelect options={langOptions} /> */}
       </HeaderWrapper>
+      <ContentWrapper>
+        <LeftContent>
+          <OverlayBg></OverlayBg>
+          <BgAnimation>
+            <ImageContainer>
+              {imageList.map((item, index) => (
+                <SubscribeImage
+                  key={index}
+                  src={item.image}
+                  alt="subscribe"
+                  width={120}
+                  height={160}
+                />
+              ))}
+            </ImageContainer>
+          </BgAnimation>
+          <SubscribeTextContent>
+            <Title>Subscribe now and start streaming</Title>
+            <Instruction>
+              You will be able to watch only on Mobile app
+            </Instruction>
+          </SubscribeTextContent>
+        </LeftContent>
+        {/* <RightContent>
+          <Table>
+            <col
+              style={{
+                width: "30%",
+              }}
+            />
+            <col
+              style={{
+                width: "20%",
+                backgroundAttachment: "fixed",
+                background: "var(--linear-gradient-color900)",
+              }}
+            />
+            <col
+              style={{
+                width: "20%",
+              }}
+            />
+            <col
+              style={{
+                width: "20%",
+              }}
+            />
+
+            <TableHeader>
+              <TableRow>
+                <Tablehead></Tablehead>
+                <Tablehead>
+                  <Heading>Mobile</Heading>
+                </Tablehead>
+                <Tablehead>
+                  <Heading>Super</Heading>
+                </Tablehead>
+                <Tablehead>
+                  <Heading>Premium</Heading>
+                </Tablehead>
+              </TableRow>
+            </TableHeader>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </Table>
+        </RightContent> */}
+      </ContentWrapper>
     </Wrapper>
   );
 }
@@ -90,7 +288,6 @@ const Wrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
   position: relative;
 `;
 const HeaderWrapper = styled.div`
@@ -209,4 +406,103 @@ const Option = styled.button`
   font-size: 16px;
   font-family: var(--FONT-FAMILY);
   cursor: pointer;
+`;
+const ContentWrapper = styled.div`
+  display: flex;
+`;
+const LeftContent = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 40%;
+  z-index: 1;
+  height: 40rem;
+`;
+const OverlayBg = styled.div`
+  background: var(--overlay-color-2);
+  width: 100%;
+  min-height: 100%;
+`;
+const BgAnimation = styled.div`
+  height: 100%;
+  position: absolute;
+  top: 0;
+  width: 99%;
+  z-index: -2;
+`;
+const ImageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  animation: imagetransition 20s linear infinite alternate;
+  @keyframes imagetransition {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-50%);
+    }
+  }
+`;
+const SubscribeImage = styled(ImageView)`
+  object-fit: cover;
+  border-radius: 4px;
+  margin: 4px;
+`;
+const SubscribeTextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  z-index: 3;
+  top: 0;
+  padding-top: 1.5rem;
+  margin-left: 5rem;
+`;
+const Title = styled.p`
+  font-family: var(--FONT-FAMILY);
+  color: var(--text-color900);
+  font-size: 38px;
+  font-weight: 700;
+`;
+const Instruction = styled.p`
+  -webkit-line-clamp: 1;
+  font-family: var(--FONT-FAMILY);
+  color: var(--gold-color-2);
+  font-size: 18px;
+  font-weight: 500;
+  margin-top: 1rem;
+`;
+const RightContent = styled.div`
+  width: 60%;
+  margin-left: 6%;
+  margin-right: 2rem;
+`;
+const Table = styled.table`
+  width: 100%;
+`;
+const Contentcolumn = styled.col`
+  width: 30%;
+`;
+const Mobilecolumn = styled.col`
+  width: 20%;
+  background-attachment: fixed;
+  background: var(--linear-gradient-color900);
+`;
+const Supercolumn = styled.col`
+  width: 20%;
+`;
+const Premuimcolumn = styled.col`
+  width: 20%;
+`;
+const TableHeader = styled.thead``;
+const TableRow = styled.tr``;
+const Tablehead = styled.thead`
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+  padding-top: 0.5rem;
+`;
+const Heading = styled.p`
+  color: var(--text-color500);
+  font-size: 18px;
+  font-weight: 700;
+  font-family: var(--FONT-FAMILY);
 `;
