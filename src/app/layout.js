9d5d855cb97dd52e4da2cb74cde75de8b2f366fc
Pragnya sahu <pragnya110@gmail.com>
2024-04-11 11:38:13 +0530

    Analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Providers from "./Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children} <Analytics />
+        </Providers>
       </body>
     </html>
   );
